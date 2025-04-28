fba550702642b69dcc36d0b8ef2a2032bbc15e8e
Luke Herbelin <lukeherbelin@ucsb.edu>
2025-04-28 16:40:32 -0700

    fibonacci

--- fibonacci.asm
+++ fibonacci.asm
@@ -31,17 +31,60 @@ main:
 
 
 # Print the prompt and get user input
-	
+	la $a0, prompt
+	li $v0, 4
+	syscall
+	li $v0, 5
+	syscall	
 # Initialize all registers needed in the program (as appropriate)
-
+	move $t0, $v0
+	li $t1, 1
+	li $t2, 1
+	li $t3, 0
+	li $t4, 2
 # if (exit conditions are met) then go to exit
+	li $t5, 1
+	ble $t0, $t5, exit
 # otherwise:
     # print "The first " n " numbers in the Fibonacci series are:\n"
+	la $a0, first
+	li $v0, 4
+	syscall
+	move $a0, $t0
+	li $v0, 1
+	syscall
+	la $a0, numbers
+	li $v0, 4
+	syscall
     # print f1 " " f2
-
+	li $v0, 1
+	move $a0, $t1
+	syscall
+	la $a0, spacech
+	li $v0, 4
+	syscall
+	li $v0, 1
+	move $a0, $t2
+	syscall	
     # while loop:
-
+loop:
+	ble $t0, $t4, exit
+	add $t3, $t1, $t2
+	move $t1, $t2
+	move $t2, $t3
+	la $a0, spacech
+	li $v0, 4
+	syscall
+	move $a0, $t3
+	li $v0, 1
+	syscall
+	addi $t4, $t4, 1	
+	j loop
 exit:
 # print newline
-
+	la $a0, newline
+	li $v0, 4
+	syscall
 # quit program
+	li $v0, 10
+	syscall
